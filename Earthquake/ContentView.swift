4efa3780a4afeda079b734e29fd66d86b7d0328a
HyeseongMo <hyeseong0914@naver.com>
2023-12-01 16:48:52 +0900

    Initial Commit

---
+++ Earthquake/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Earthquake
+//
+//  Created by Paul Mo on 2023/12/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Earthquake/EarthquakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakeApp.swift
+//  Earthquake
+//
+//  Created by Paul Mo on 2023/12/01.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
